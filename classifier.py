a36a2b173a54f11d6bf4b9a60bc085af62ca6151
kich <victorkich@yahoo.com.br>
2023-09-22 18:42:16 +0900

    Update

--- classifier.py
+++ classifier.py
@@ -4,7 +4,7 @@ import numpy as np
 from ultralytics import YOLO
 
 model = YOLO('best.pt')
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(2)
 
 # For calculating FPS
 font = cv2.FONT_HERSHEY_SIMPLEX
