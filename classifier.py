b776f83619d375f8f5816d9506ca5bbd582e829f
kich <victorkich@yahoo.com.br>
2023-09-22 18:38:28 +0900

    Update

--- classifier.py
+++ classifier.py
@@ -6,10 +6,18 @@ from ultralytics import YOLO
 model = YOLO('best.pt')
 cap = cv2.VideoCapture(0)
 
+# For calculating FPS
+font = cv2.FONT_HERSHEY_SIMPLEX
+color = (0, 255, 0)  # Green
+font_scale = 0.7
+thickness = 2
+prev_frame_time = 0
+
 # Loop through the video frames
 while cap.isOpened():
     # Read a frame from the video
     success, frame = cap.read()
+    new_frame_time = cv2.getTickCount()
 
     if success:
         # Run YOLOv8 inference on the frame
@@ -17,6 +25,14 @@ while cap.isOpened():
 
         # Visualize the results on the frame
         annotated_frame = results[0].plot()
+
+        # Calculate FPS
+        fps = cv2.getTickFrequency() / (new_frame_time - prev_frame_time)
+        prev_frame_time = new_frame_time
+        fps_text = f"FPS: {fps:.2f}"
+
+        # Put the FPS on the top-left corner of the frame
+        cv2.putText(annotated_frame, fps_text, (10, 30), font, font_scale, color, thickness)
 
         # Display the annotated frame
         cv2.imshow("YOLOv8 Inference", annotated_frame)
