dbdfaf95c8106344ae87a724b99e569e3dc43abc
kich <victorkich@yahoo.com.br>
2023-09-22 18:53:14 +0900

    Update

--- classifier.py
+++ classifier.py
@@ -1,48 +1,43 @@
 import cv2
+import numpy as np
 from ultralytics import YOLO
 
+def detect_cameras(max_cameras=10):
+    """Detecta as câmeras conectadas ao sistema e retorna seus índices."""
+    connected_cameras = []
+    
+    # Tenta inicializar VideoCapture para vários índices
+    for i in range(max_cameras):
+        cap = cv2.VideoCapture(i)
+        if cap.isOpened():
+            # A câmera está conectada
+            connected_cameras.append(i)
+            cap.release()
+    
+    return connected_cameras
+
+# Detecta as câmeras conectadas
+camera_ids = detect_cameras()
+captures = [cv2.VideoCapture(id) for id in camera_ids]
+
+# Inicializa o modelo YOLO
 model = YOLO('best.pt')
 
-# Abre três câmeras. Seus índices podem variar. Aqui estou assumindo 0, 1 e 2.
-cap1 = cv2.VideoCapture(2)
-cap2 = cv2.VideoCapture(3)
-cap3 = cv2.VideoCapture(4)
+while all([cap.isOpened() for cap in captures]):
+    frames = [cap.read()[1] for cap in captures]
+    results = [model(frame) for frame in frames]
+    annotated_frames = [res[0].plot() for res in results]
 
-font = cv2.FONT_HERSHEY_SIMPLEX
-color = (0, 255, 0)  # Verde
-font_scale = 0.7
-thickness = 2
+    # Combina as imagens lado a lado
+    combined_frame = np.hstack(annotated_frames)
+    
+    cv2.imshow("YOLOv8 Inference", combined_frame)
 
-while True:
-    # Lê frames das três câmeras
-    success1, frame1 = cap1.read()
-    success2, frame2 = cap2.read()
-    success3, frame3 = cap3.read()
+    # Break the loop if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
 
-    # Verifica se todos os frames foram capturados com sucesso
-    if success1 and success2 and success3:
-        # Processa cada frame com o modelo YOLO
-        results1 = model(frame1)
-        results2 = model(frame2)
-        results3 = model(frame3)
-
-        # Visualiza os resultados em cada frame
-        annotated_frame1 = results1[0].plot()
-        annotated_frame2 = results2[0].plot()
-        annotated_frame3 = results3[0].plot()
-
-        # Combina os frames horizontalmente
-        combined_frame = cv2.hconcat([annotated_frame1, annotated_frame2, annotated_frame3])
-
-        # Mostra o frame combinado
-        cv2.imshow("YOLOv8 Inference", combined_frame)
-
-        # Encerra o loop se 'q' for pressionado
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-# Libera os objetos de captura e fecha todas as janelas
-cap1.release()
-cap2.release()
-cap3.release()
+# Libera os objetos VideoCapture e fecha todas as janelas
+for cap in captures:
+    cap.release()
 cv2.destroyAllWindows()
